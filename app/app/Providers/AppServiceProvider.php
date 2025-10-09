d824ae27e657f04b8784dca128419e5f0156ff45
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2025-10-09 11:04:11 +0300

    minor

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -5,10 +5,18 @@ namespace App\Providers;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Application service provider.
+ *
+ * This service provider is responsible for registering and bootstrapping
+ * application-specific services and configurations.
+ */
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
     public function register(): void
     {
@@ -17,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
     public function boot(): void
     {
