13ffb2f452cd2e01513996c3f80cfe88f440bd1b
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2025-10-09 10:06:09 +0300

    install breeze

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -2,20 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
-/**
- * Application service provider.
- * 
- * This service provider is responsible for registering and bootstrapping
- * application-specific services and configurations.
- */
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     * 
-     * @return void
      */
     public function register(): void
     {
@@ -24,11 +17,11 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     * 
-     * @return void
      */
     public function boot(): void
     {
-        //
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
     }
 }
--- app/routes/api.php
+++ app/routes/api.php
@@ -1,23 +1,8 @@
 <?php
 
-use App\Modules\City\Routes\CityRoutes;
-use App\Modules\Play\Routes\PlayRoutes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-// API v1 routes
-Route::prefix('v1')
-    ->as('v1.')
-    ->middleware('auth:sanctum')
-    ->group(function () {
-        // City routes
-        CityRoutes::register();
-
-        // Play routes
-        PlayRoutes::register();
-    })
-;
+});
